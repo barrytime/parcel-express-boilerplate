49078a1c19901cb35090d5ad89dd70a113dcbfd4
barrytime <dan.barry22@gmail.com>
2020-07-05 13:10:02 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var apiRouter = require('./routes/api/index');
+var testRouter = require('./routes/api/test');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/api', apiRouter);
+app.use('/api/test', testRouter);
+
+module.exports = app;
